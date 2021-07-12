5afe8d864440c57346a25f8d88d7a6bcf5daf032
ironblockhd <69308275+ironblockhd@users.noreply.github.com>
2021-07-12 09:25:21 +0200

    port breeze dark icons

---
+++ assets/dirConvert.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+inputfile=$1;
+echo $inputfile > /dev/stderr;
+parsed=$(cat $inputfile|jq -R -s '.');
+echo -n "\"$(basename $inputfile)\":{meta:{changeDate:0,owner:\"root\",permission:[7,5,5],type:\"file\"},content:$parsed},"
