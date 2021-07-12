5772f78f5a02a512fa71c65d80c0bd4733f65260
ironblockhd <69308275+ironblockhd@users.noreply.github.com>
2021-07-12 21:35:00 +0200

    add more application icons

--- assets/dirConvert.sh
+++ assets/dirConvert.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+#usage: 
 inputfile=$1;
 echo $inputfile > /dev/stderr;
 parsed=$(cat $inputfile|jq -R -s '.');
-echo -n "\"$(basename $inputfile)\":{meta:{changeDate:0,owner:\"root\",permission:[7,5,5],type:\"file\"},content:$parsed},"
+echo -n "\"$(basename $inputfile)\":{\"meta\":{\"changeDate\":0,\"owner\":\"root\",\"permission\":[7,5,5],\"type\":\"file\"},\"content\":$parsed},"
