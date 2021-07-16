d45a40e46192805a770a2143452cd8d24ff6781f
ironblockhd <69308275+ironblockhd@users.noreply.github.com>
2021-07-16 20:21:13 +0200

    clean up & comment desktop.js

--- js/desktop.js
+++ js/desktop.js
@@ -7,15 +7,21 @@ import DesktopDrag from "./desktopDrag.js";
 import "./windowmanager.js"
 import * as linux from "../linuxCore/index.js";
 import ThemeLoader from "./themeparser.js"
+
 let config;
+
 class Desktop {
     constructor(config) {
-        this.config = config;
-        this.theme = new ThemeLoader(linux.fileapi.internal.read("/usr/share/themes/"+this.config.desktop.theme),"data:application/octet-stream;base64,"+linux.fileapi.internal.read(this.config.font));
+
         this.apps = [];
         this.selectedApps = [];
         this.panels = [];
+
+        this.config = config;
+        // load current theme from config, with font
+        this.theme = new ThemeLoader(linux.fileapi.internal.read("/usr/share/themes/" + this.config.desktop.theme), "data:application/octet-stream;base64," + linux.fileapi.internal.read(this.config.font));
         this.element = document.getElementById("desktop");
+
         this.render();
         this.addListeners()
     }
@@ -24,62 +30,76 @@ class Desktop {
         this.renderApps();
         this.renderPanels();
     };
+    // (re-)render icons on desktop
     renderApps() {
+        //clear all icons
         this.apps.forEach(app => app.remove());
+
+        // get list of file names on desktop
         let apps = linux.fileapi.internal.list("/home/demo/Desktop");
+        // generate appropriate app elements using the name 
         let preparedApps = apps.map(function (app, index, apps) {
             let result = {};
             result.name = app;
+            // attempt to set correct icon using mime type
             result.icon = "/usr/share/icons/breeze-dark/mimetypes/" + toMime(app).replace("/", "-") + ".svg";
+            //position in desktop grid
             result.position = { x: 0, y: 0 };
             return result;
         })
+        // generate app icons and store them in desktop instance
         preparedApps.forEach(element => {
             this.apps.push(new DesktopApp(element.name, element.icon, element.position, config.apps));
         });
     }
+    // grab panels from config and render them
     renderPanels() {
         this.panels.forEach(panel => panel.remove());
         this.config.desktop.panels.forEach(panel => {
             this.panels.push(new Panel(panel));
         })
     }
+    // add all event listeners needed
     addListeners() {
+
         this.element.addEventListener("contextmenu", event => {
-            event.preventDefault()
-            if (event.target.id != "desktop") {
+            // prevent browser context menu
+            event.preventDefault();
+            // clicked another element. Let it handle it
+            if (event.target != this.element) {
                 return;
             }
+            // spawn context menu
             new DesktopMenu({ x: event.pageX, y: event.pageY }, [{
                 text: 'Create new...',
-                icon:"/usr/share/icons/breeze-dark/actions/document-new.svg",
-                submenus:[{
-                    text:"Folder",
-                    icon:"/usr/share/icons/breeze-dark/actions/folder-new.svg"
-                },{
-                    text:"Text Document",
-                    icon:"/usr/share/icons/breeze-dark/actions/x-shape-text.svg"
+                icon: "/usr/share/icons/breeze-dark/actions/document-new.svg",
+                submenus: [{
+                    text: "Folder",
+                    icon: "/usr/share/icons/breeze-dark/actions/folder-new.svg"
+                }, {
+                    text: "Text Document",
+                    icon: "/usr/share/icons/breeze-dark/actions/x-shape-text.svg"
                 }]
             },
             {
-                text:"Open with file manager",
-                icon:"/usr/share/icons/breeze-dark/apps/system-file-manager.svg"
+                text: "Open with file manager",
+                icon: "/usr/share/icons/breeze-dark/apps/system-file-manager.svg"
             },
             {
                 text: "Refresh desktop",
                 icon: "/usr/share/icons/breeze-dark/places/desktop.svg",
-                action: ()=> { this.render() }
+                action: () => { this.render() }
             }]);
         });
+        // track mousedown position to allow drag click selection later
+
         this.element.addEventListener("mousedown", event => {
             if (event.target == this.element) {
                 this.mousedown = true;
                 this.mousepos = { x: event.pageX, y: event.pageY };
             }
-            if(event.target.classList.contains("app")||event.target.classList.contains("appicon")||event.target.classList.contains("appname")){
-
-            }
         });
+        // when the mouse is released and the user is performinng drag selection, stop it
         this.element.addEventListener("mouseup", event => {
             this.mousedown = false;
             if (this.drag) {
@@ -87,42 +107,48 @@ class Desktop {
                 this.drag = null;
             }
         })
+        // mouse was moved
         this.element.addEventListener("mousemove", event => {
-            if (!this.mousedown || (!this.drag&&event.target != this.element&&!event.target.classList.contains("desktopdrag"))) {
+            // mouse button wasn't down. Nothing to do.
+            // Or: bubbled up from other element. Let it handle it.
+            if (!this.mousedown || (!this.drag && event.target != this.element && !event.target.classList.contains("desktopdrag"))) {
                 return;
             }
+            // Not currently utilizing click drag selection. Start to.
             if (!this.drag) {
                 this.drag = new DesktopDrag(this.mousepos, { x: event.pageX, y: event.pageY });
             }
+            // Currently dragging. Report position change to respective drag instance.
             if (this.drag) {
                 this.drag.change({ x: event.pageX, y: event.pageY });
             }
         });
-        window.addEventListener("touchstart",event=>{
+        // Translate various touch events for a (hacky) mobile suppot
+        window.addEventListener("touchstart", event => {
             document.body.requestFullscreen();
-            let node = event.target;
             let clickEvent = document.createEvent('MouseEvents');
-            clickEvent.initMouseEvent("mousedown",true,true,window,0,event.touches[0].screenX,event.touches[0].screenY,event.touches[0].clientX,event.touches[0].clientY);
+            clickEvent.initMouseEvent("mousedown", true, true, window, 0, event.touches[0].screenX, event.touches[0].screenY, event.touches[0].clientX, event.touches[0].clientY);
             event.target.dispatchEvent(clickEvent);
         });
-        window.addEventListener("touchmove",event=>{
-            let node = event.target;
+        window.addEventListener("touchmove", event => {
             let clickEvent = document.createEvent('MouseEvents');
             this.lastTouch = event;
-            clickEvent.initMouseEvent("mousemove",true,true,window,0,event.touches[0].screenX,event.touches[0].screenY,event.touches[0].clientX,event.touches[0].clientY);
+            clickEvent.initMouseEvent("mousemove", true, true, window, 0, event.touches[0].screenX, event.touches[0].screenY, event.touches[0].clientX, event.touches[0].clientY);
             event.target.dispatchEvent(clickEvent);
         });
-        window.addEventListener("touchend",()=>{
+        window.addEventListener("touchend", () => {
             event = this.lastTouch;
-            let node = event.target;
             let clickEvent = document.createEvent('MouseEvents');
-            clickEvent.initMouseEvent("mouseup",true,true,window,0,event.touches[0].screenX,event.touches[0].screenY,event.touches[0].clientX,event.touches[0].clientY);
+            clickEvent.initMouseEvent("mouseup", true, true, window, 0, event.touches[0].screenX, event.touches[0].screenY, event.touches[0].clientX, event.touches[0].clientY);
             event.target.dispatchEvent(clickEvent);
         });
     }
 };
-linux.fileapi.onready.then(()=>{
-config = JSON.parse(linux.fileapi.internal.read("/home/demo/.config/plasma.json"));
-window.desktop = new Desktop(config);
-localStorage.downloaded = true;
+// wait for file system to load and render desktop
+linux.fileapi.onready.then(() => {
+    // start config and
+    config = JSON.parse(linux.fileapi.internal.read("/home/demo/.config/plasma.json"));
+    window.desktop = new Desktop(config);
+    // set flag to not download filesystem assets on next reload
+    localStorage.downloaded = true;
 });
