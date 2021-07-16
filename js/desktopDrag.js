c7936c8821cc04922e2ccd3d89a1094d6fcd0ce9
ironblockhd <69308275+ironblockhd@users.noreply.github.com>
2021-07-16 20:31:01 +0200

    clean up & comment desktopDrag.js

--- js/desktopDrag.js
+++ js/desktopDrag.js
@@ -1,39 +1,44 @@
 "use strict";
-class DesktopDrag{
-    constructor(startPosition, currentPosition){
+class DesktopDrag {
+    // Initiate drag with position objects {x:xPos,y:yPos}
+    constructor(startPosition, currentPosition) {
         this.startPosition = startPosition;
         this.currentPosition = currentPosition;
     }
-    render(){
-        this.element = this.element;
-        if(!this.element){
-        this.element = document.createElement("div");
-        this.element.classList.add("desktopdrag");
-        document.getElementById("desktop").appendChild(this.element)
+    render() {
+        // Generate element if it doesn't exist
+        if (!this.element) {
+            this.element = document.createElement("div");
+            this.element.classList.add("desktopdrag");
+            document.getElementById("desktop").appendChild(this.element);
         }
-        if(this.currentPosition.y - this.startPosition.y > 0){
+        // Normal drag. Change width to fit change
+        if (this.currentPosition.x - this.startPosition.x > 0) {
+            this.element.style.left = this.startPosition.x + "px";
+            this.element.style.width = (this.currentPosition.x - this.startPosition.x) + "px";
+        }
+        // Drag backwards (current position nearer to screen start than starting position)
+        else {
+            this.element.style.left = this.currentPosition.x + "px";
+            this.element.style.width = (this.startPosition.x - this.currentPosition.x) + "px";
+        }
+        // Normal drag. Change height to fit change
+        if (this.currentPosition.y - this.startPosition.y > 0) {
             this.element.style.top = this.startPosition.y + "px";
             this.element.style.height = (this.currentPosition.y - this.startPosition.y) + "px";
         }
-        else{
-            this.element.style.top =  this.currentPosition.y + "px";
-            this.element.style.height =  (this.startPosition.y - this.currentPosition.y) + "px";
-        }
-        if(this.currentPosition.x - this.startPosition.x > 0){
-            this.element.style.left = this.startPosition.x + "px";
-            this.element.style.width = (this.currentPosition.x - this.startPosition.x) + "px";   
-        }
-        else{
-            this.element.style.left = this.currentPosition.x + "px";
-            this.element.style.width = (this.startPosition.x - this.currentPosition.x) + "px";  
+        // Drag backwards (current position nearer to screen start than starting position)
+        else {
+            this.element.style.top = this.currentPosition.y + "px";
+            this.element.style.height = (this.startPosition.y - this.currentPosition.y) + "px";
         }
     }
-    remove(){
+    remove() {
         this.element.outerHTML = "";
     }
-    change(position){
+    change(position) {
         this.currentPosition = position;
         this.render();
     }
 }
-export {DesktopDrag as default};
+export { DesktopDrag as default };
