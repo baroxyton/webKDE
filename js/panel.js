9872a5b94f5594eae0df3167dd85ce5a298d54e1
ironblockhd <69308275+ironblockhd@users.noreply.github.com>
2021-07-17 07:33:37 +0200

    comment themeParser.js

--- js/panel.js
+++ js/panel.js
@@ -1,33 +1,37 @@
-import * as widgets from "./widgets.js"; 
+import * as widgets from "./widgets.js";
 "use strict";
-class Panel{
-    constructor(config){
+class Panel {
+    constructor(config) {
         this.widgets = [];
         this.config = config;
         this.render();
     }
-    render(){
+    // Render widgets and dimensions from config
+    render() {
         this.remove();
 
         this.panelElement = document.createElement("div");
         this.panelElement.classList.add("panel");
+
         this.panelElement.style.height = this.config.height + "px";
         this.panelElement.style.width = this.config.width + "%";
         this.panelElement.style.left = this.config.left + "%";
         this.panelElement.top = this.config.top + "%";
-        this.config.items.forEach(item=>{
+
+        this.config.items.forEach(item => {
             let newWidget = new widgets[item.type](this.panelElement);
-            if(item.styles){
+            if (item.styles) {
                 newWidget.align(item.styles);
             }
             this.widgets.push(newWidget);
         })
         document.getElementById("desktop").appendChild(this.panelElement);
     }
-    remove(){
-        if(this.panelElement){
-        this.panelElement.outerHTML = "";
+    
+    remove() {
+        if (this.panelElement) {
+            this.panelElement.outerHTML = "";
         }
     }
 }
-export {Panel as default};
+export { Panel as default };
