99ec97900dca124efbc2f852b713177625c04a2b
ironblockhd <69308275+ironblockhd@users.noreply.github.com>
2021-07-17 08:34:02 +0200

    clean up & comment windowmanager.js

--- js/windowmanager.js
+++ js/windowmanager.js
@@ -1,26 +1,32 @@
 "use strict";
+
+// Window manager class
+
 class WebKWin{
     constructor(url,theme){
         this.url = url;
         this.theme = theme;
+
         this.render();
     }
+
     render(){
         this.element = document.createElement("div");
-        this.element.classList.add("kwin");
         this.navbar = document.createElement("div");
-        this.navbar.classList.add("knavbar");
         this.title = document.createElement("div");
-        this.title.classList.add("kwintitle");
-        this.title.innerText = "test"
         this.icon = document.createElement("div");
-        this.icon.classList.add("kwinIcon");
-        this.icon.style.backgroundImage = `url("data:image/svg+xml;base64,${btoa(debug.fileapi.internal.read("/usr/share/icons/breeze-dark/apps/com.visualstudio.code.oss.svg"))}")`;
         let actions = document.createElement("div");
-        actions.innerHTML = `<div class="minimizeIcon"></div>
-        <div class="maximizeIcon"></div>
-        <div class="closeIcon"></div>`;
+
+        this.element.classList.add("kwin");
+        this.navbar.classList.add("knavbar");
+        this.title.classList.add("kwintitle");
         actions.classList.add("kwinActions");
+        this.icon.classList.add("kwinIcon");
+
+        this.title.innerText = "test"
+        this.icon.style.backgroundImage = `url("data:image/svg+xml;base64,${btoa(debug.fileapi.internal.read("/usr/share/icons/breeze-dark/apps/com.visualstudio.code.oss.svg"))}")`;
+        actions.innerHTML = `<div class="minimizeIcon"></div><div class="maximizeIcon"></div><div class="closeIcon"></div>`;
+
         this.navbar.appendChild(actions);
         this.navbar.appendChild(this.icon);
         this.navbar.appendChild(this.title);
