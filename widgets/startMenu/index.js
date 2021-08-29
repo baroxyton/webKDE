6a916e76baad2f1e7acc875367379cc45ed307b7
ironblockhd <69308275+ironblockhd@users.noreply.github.com>
2021-08-29 21:42:53 +0200

    Basic category render

--- widgets/startMenu/index.js
+++ widgets/startMenu/index.js
@@ -18,7 +18,8 @@ let categories = [
     "other"
 ];
 let assignedApplications = {};
-function parseApp(data){
+let renderedCategories = [];
+function parseApp(data) {
     let result = {};
     let sections = data.split("\n\n");
     // Iterate over sections
@@ -44,26 +45,48 @@ function parseApp(data){
     })
     return result;
 }
-async function updateApps(){
-    let appData = [];
-    let appList =  (await api.filesystem("list", "/usr/share/applications")).read().content;
-    appData = await Promise.all(appList.map(async app=>(await api.filesystem("read", "/usr/share/applications/"+app)).read().content));
+
+class Category{
+    constructor(category){
+        this.category = category;
+        this.render();
+    }
+    render(){
+        renderedCategories.push(this);
+        this.element = document.createElement("div");
+        this.element.setAttribute("icon", "/usr/share/icons/breeze-dark/actions/draw-arrow-forward.svg");
+        this.iconElement = document.createElement("div");
+        this.iconElement.setAttribute("icon",`/usr/share/icons/breeze-dark/categories/applications-${this.category}.svg`);
+        this.element.innerText = this.category;
+        this.element.appendChild(this.iconElement);
+        document.getElementById("categories").appendChild(this.element);
+    }
+    remove(){
+        this.element.outerHTML = "";
+    }
+}
+async function updateApps() {
     assignedApplications = {};
-    appData.forEach(data=>{
+    renderedCategories.forEach(category=>category.remove());
+    let appData = [];
+    let appList = (await api.filesystem("list", "/usr/share/applications")).read().content;
+    appData = await Promise.all(appList.map(async app => (await api.filesystem("read", "/usr/share/applications/" + app)).read().content));
+    appData.forEach(data => {
         let parsedFile = parseApp(data);
         let name = parsedFile["Desktop Entry"].Name[0];
         let cmd = parsedFile["Desktop Entry"].Exec[0];
         let icon = parsedFile["Desktop Entry"].Icon[0];
-        let fileCategories = parsedFile["Desktop Entry"].Categories||[];
-        let app = {name, cmd, icon};
-        categories.forEach(category=>{
-            if(fileCategories.includes(category)||category=="all"){
-                assignedApplications[category] = assignedApplications[category]||[];
+        let fileCategories = parsedFile["Desktop Entry"].Categories || [];
+        let app = { name, cmd, icon };
+        categories.forEach(category => {
+            if (fileCategories.includes(category) || category == "all") {
+                assignedApplications[category] = assignedApplications[category] || [];
                 assignedApplications[category].push(app);
+                api.loadIcons();
             }
         });
     });
-    console.log(assignedApplications);
+    Object.keys(assignedApplications).forEach(category=>new Category(category))
 }
 api.channel.onevent = data => {
     switch (data.event) {
