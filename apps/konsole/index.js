8dcc664495b0fe58ac6dc4a277c8aad420af9d5d
ironblockhd <69308275+ironblockhd@users.noreply.github.com>
2021-09-27 14:02:15 +0200

    Make konsole functional

--- apps/konsole/index.js
+++ apps/konsole/index.js
@@ -1,7 +1,54 @@
 import OSApi from "../../appApi/frontend/api.js";
+import { data } from "../../linuxCore/index.js";
 let api = new OSApi();
 let inputContent = "";
 let caretPosition = 0;
+let ttyLocation = "/";
+let user = "demo";
+let commandIsRunning = false;
+let preinput = "demo@linux:/ $";
+async function updatePreinput() {
+    user = api.data.user;
+    ttyLocation = (await api.readEnv("PWD")).read();
+    preinput = `${user}@linux:${ttyLocation.replace((await api.readEnv("HOME", "~")).read())} $`;
+    document.getElementById("user").innerText = preinput;
+}
+function runCommand(command) {
+    commandIsRunning = true;
+    document.getElementById("user").innerText = "";
+    let isDone = false;
+    api.tty.onData.add(function (data) {
+        if (isDone) {
+            return;
+        }
+        switch (data.type) {
+            case "io":
+                if (data.io == "stdout" || data.io == "stderr") {
+                    if (data.event == "write") {
+                        addToLog(data.data);
+                    }
+                    if (data.data == "{{{clear}}}") {
+                        document.getElementById("content").innerHTML = "";
+                    }
+                }
+                if (data.io == "stdin") {
+                    inputContent += data.data;
+                    if (data.data == "{{{clear}}}") {
+                        inputContent = "";
+                        caretPosition = 0;
+                    }
+                    loadInputContent();
+                }
+            case "process":
+                if (data.event == "quit") {
+                    isDone = true;
+                    commandIsRunning = false;
+                    updatePreinput();
+                }
+        }
+    });
+    api.tty.run(command);
+}
 function loadInputContent() {
     document.getElementById("input").innerHTML = "";
     let chars = (inputContent + " ").split("");
@@ -14,12 +61,16 @@ function loadInputContent() {
         document.getElementById("input").appendChild(element);
     });
 }
-function addToLog(text){
-document.getElementById("content").innerText += text;
-document.body.scrollTop = document.body.scrollHeight;
+function addToLog(text) {
+    document.getElementById("content").innerText += text;
+    document.body.scrollTop = document.body.scrollHeight;
 }
 
 document.body.addEventListener("keydown", event => {
+    event.preventDefault();
+    if (commandIsRunning) {
+        api.tty.sendKey(event);
+    }
     let key = event.key;
     switch (key) {
         case "ArrowLeft":
@@ -43,10 +94,16 @@ document.body.addEventListener("keydown", event => {
             loadInputContent();
             break;
         case "Enter":
-            addToLog(inputContent + "\n");
+            let command = inputContent;
+            addToLog(`${preinput} ${inputContent}\n`);
             inputContent = "";
             caretPosition = 0;
             loadInputContent();
+            if (commandIsRunning) {
+                api.tty.sendIo("stderr", inputContent);
+                return;
+            }
+            runCommand(command)
             break;
         default:
             if (key.length > 1) {
@@ -73,5 +130,10 @@ api.gotData.then(async () => {
         title: "Konsole",
         icon: "/usr/share/icons/breeze-dark/apps/utilities-terminal.svg"
     });
+    setTimeout(a => api.tty.run("ls"), 1000);
+    await updatePreinput();
 });
+api.tty.onData.add(function (data) {
+    console.log(data)
+})
 loadInputContent();
